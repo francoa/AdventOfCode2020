7946a5d17af2665b1c663ba533eaf1f9ee65f6a7
Franco Ardiani <francoamg@gmail.com>
2020-12-07 01:17:04 +0100

    Update Puzzle_Day_1.py

--- Puzzle_Day_1.py
+++ Puzzle_Day_1.py
@@ -1,18 +1,23 @@
 import math
+import unittest
 
-entries = []
-try:
-    with open('input_puzzle_1', 'r') as input_file:
-        number = input_file.readline()
-        while number:
-            entries.append(int(number))
-            number = input_file.readline()
-except Exception as exc:
-    print(f"There was an exception: {exc}")
+from import_lines import import_data_to_array
+
+
+def process_puzzle_data(input_file):
+    lines = import_data_to_array(input_file)
+    entries = []
+    try:
+        for line in lines:
+            entries.append(int(line))
+    except Exception as exc:
+        print(f"There was an exception: {exc}")
+    return entries
+
 
 def find_values(entries, index, value_remaining, number_remaining):
     return_value = []
-    
+
     while index < len(entries):
         current_value = entries[index]
 
@@ -21,20 +26,51 @@ def find_values(entries, index, value_remaining, number_remaining):
             if len(return_value):
                 return_value.append(current_value)
                 break
-        
+
         elif current_value == value_remaining and number_remaining == 1:
             return_value.append(current_value)
             break
-        
+
         index = index + 1
 
     return return_value
 
 
-number_of_values_to_find = 3 
-result = find_values(entries, 0, 2020, number_of_values_to_find)
+def solve_puzzle_part_1():
+    data = process_puzzle_data('input_puzzle_1')
+    result = find_values(data, 0, 2020, 2)
 
-if len(result) == number_of_values_to_find:
-    print(f"Results {result}. Multiplication: {math.prod(result)}")
-else:
-    print("Couldn't find value")
+    if len(result) == 2:
+        print(f"Results {result}. Multiplication: {math.prod(result)}")
+    else:
+        print("Couldn't find value")
+
+
+def solve_puzzle_part_2():
+    data = process_puzzle_data('input_puzzle_1')
+    result = find_values(data, 0, 2020, 3)
+
+    if len(result) == 3:
+        print(f"Results {result}. Multiplication: {math.prod(result)}")
+    else:
+        print("Couldn't find value")
+
+
+if __name__ == "__main__":
+    solve_puzzle_part_2()
+
+
+"""  TESTING  """
+
+
+class TestPuzzle1(unittest.TestCase):
+
+    def test_example_part_1(self):
+        entries = [1721, 979, 366, 299, 675, 1456]
+        result = find_values(entries, 0, 2020, 2)
+        self.assertEqual(math.prod(result), 514579)
+
+    def test_example_part_2(self):
+        entries = [1721, 979, 366, 299, 675, 1456]
+        result = find_values(entries, 0, 2020, 3)
+        self.assertEqual(math.prod(result), 241861950)
